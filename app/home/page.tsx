be07cf4651a9a5be7f5c3b70f5ae42b93ae1afd5
Thomas Pham <phamthomas19@gmail.com>
2025-06-09 01:01:59 -0500

    add padding on sides of pages

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -10,7 +10,6 @@ import LgNav from '../components/nav/LgNav';
 import NameBlock from '../components/blocks/NameBlock';
 import BlogBlock from '../components/blocks/BlogBlock';
 import EmailBlock from '../components/blocks/EmailBlock';
-// import TechnologiesBlock from '../components/blocks/TechStackBlock';
 import TechnologyBlock from '../components/blocks/techStackBlock/TechnologyBlock';
 import ProjectsBlock from '../components/blocks/ProjectsBlock';
 import AboutBlock from '../components/blocks/AboutBlock';
@@ -22,56 +21,58 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export const Home = () => {
     return (
-        <div style={{height: '100vh', width: '100vw', backgroundColor: colors.black}}>
+        <div style={{ height: '100vh', width: '100vw', backgroundColor: colors.black }}>
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', fontFamily: 'Inter, sans-serif' }}>
 
                 <LgNav />
+                <div style={{width: '100%', padding: '0 16px', display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+                    <div style={{ maxWidth: '1500px', width: '100%'}}>
+                        <ResponsiveGridLayout
+                            layouts={{ lg: lg, md: md, sm: sm, xs: xs }}
+                            breakpoints={{ lg: 1450, md: 1000, sm: 500, xs: 0 }}
+                            cols={{ lg: 3, md: 2, sm: 1, xs: 1 }}
+                            rowHeight={100}
+                            isDraggable={false}
+                            isResizable={false}
+                            useCSSTransforms
+                            className="layout"
 
-                <div style={{ maxWidth: '1500px', width: '100%' }}>
-                    <ResponsiveGridLayout
-                        layouts={{ lg: lg, md: md, sm: sm, xs: xs }}
-                        breakpoints={{ lg: 1490, md: 1000, sm: 500, xs: 0 }}
-                        cols={{ lg: 3, md: 2, sm: 1, xs: 1 }}
-                        rowHeight={100}
-                        isDraggable={false}
-                        isResizable={false}
-                        useCSSTransforms
-                        className="layout"
+                            style={{}}
+                        >
+                            <div key="Name">
+                                <NameBlock />
+                            </div>
+                            <div key="TechStack">
+                                <TechnologyBlock />
+                            </div>
+                            <div key="About">
+                                <AboutBlock />
+                            </div>
+                            <div key="Picture">
+                                <CardStackBlock />
+                            </div>
 
-                        style={{}}
-                    >
-                        <div key="Name">
-                            <NameBlock />
-                        </div>
-                        <div key="TechStack">
-                            <TechnologyBlock />
-                        </div>
-                        <div key="About">
-                            <AboutBlock />
-                        </div>
-                        <div key="Picture">
-                            <CardStackBlock />
-                        </div>
-
-                        <div key="Blog">
-                            <BlogBlock />
-                        </div>
-                        <div key="Email">
-                            <EmailBlock />
-                        </div>
-                        <div key="Projects">
-                            <ProjectsBlock />
-                        </div>
-                        <div key="Spotify">
-                            <SpotifyBlock />
-                        </div>
-                        <div key="SocialMedia">
-                            <SocialMediaBlock />
-                        </div>
+                            <div key="Blog">
+                                <BlogBlock />
+                            </div>
+                            <div key="Email">
+                                <EmailBlock />
+                            </div>
+                            <div key="Projects">
+                                <ProjectsBlock />
+                            </div>
+                            <div key="Spotify">
+                                <SpotifyBlock />
+                            </div>
+                            <div key="SocialMedia">
+                                <SocialMediaBlock />
+                            </div>
 
 
-                    </ResponsiveGridLayout>
+                        </ResponsiveGridLayout>
+                    </div>
                 </div>
+
 
             </div>
         </div>
