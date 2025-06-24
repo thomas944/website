41c95eeac9780ca95a38c638dad56733966d85c3
Thomas Pham <phamthomas19@gmail.com>
2025-06-23 19:50:33 -0500

    refactor mnist page styling into seperate page

--- frontend/components/projects/mnist/MnistPage.tsx
+++ frontend/components/projects/mnist/MnistPage.tsx
@@ -6,25 +6,10 @@ import { colors } from '@/utils/colors';
 import CanvasBlock from './canvasBlock/CanvasBlock';
 import ConfusionMatrixBlock from './confusionMatrixBlock/ConfusionMatrixBlock';
 import PredictionsBlock from './predictionsBlock/PredictionsBlock';
-
+import styles from './MnistPage.module.css';
+import { lg, md } from './layouts'
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
-
-
-export const lg = [
-    { i: 'Canvas', x: 0, y: 0, w: 1, h: 4},
-    { i: 'ModelPred', x: 1, y: 0, w: 2, h: 6.75},
-    { i: 'Stats', x: 0, y: 1.5, w: 1, h: 6},
-
-
-]
-
-const md = [
-    { i: 'Canvas', x: 0, y: 0, w: 2, h: 4},
-    { i: 'ModelPred', x: 0, y: 1.5, w: 2, h: 6.75},
-    { i: 'Stats', x: 0, y: 1.5, w: 2, h: 6},
-
-]
 
 export interface Prediction {
     digit: number;
@@ -42,14 +27,14 @@ const MnistPage = () => {
     const [isLoading, setIsLoading] = useState(false)
 
     return (
-        <div style={{ minHeight: '100vh', width: '100vw', backgroundColor: ''}}>
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', fontFamily: 'Geist, sans-serif' }}>
-                <div style={{display: 'flex', flexDirection: 'column', }}>
-                    <span style={{fontSize: '4.5rem', fontWeight: 800}}>Interactive Mnist Explorer</span>
-                    <span style={{fontSize: '1.5rem', color: '#666666', lineHeight: 1}}>Draw digits on the canvas and watch an AI model guess what it is</span>
+        <div className={styles.container}>
+            <div className={styles.titleContainer}>
+                <div className={styles.headerContainer}>
+                    <span className={styles.topHeader}>Interactive Mnist Explorer</span>
+                    <span className={styles.subHeader}>Draw digits on the canvas and watch an AI model guess what it is</span>
                 </div>
-                <div style={{width: '100%', padding: '32px 16px', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-                    <div style={{maxWidth: '950px', width: '100%', backgroundColor: '#FCFCFC', borderRadius: '1rem', border: '1px solid #eaeaea'}}>
+                <div className={styles.subContainer}>
+                    <div className={styles.contentContainer}>
                         <ResponsiveGridLayout
                             layouts={{lg: lg, md: md}}
                             breakpoints={{lg: 900, md: 500}}
@@ -70,12 +55,9 @@ const MnistPage = () => {
                                 <ConfusionMatrixBlock />
                             </div>
                         </ResponsiveGridLayout>
-
                     </div>
-
                 </div>
             </div>
-
         </div>
     )
 }
---
+++ frontend/components/projects/mnist/layouts.ts
@@ -0,0 +1,14 @@
+
+export const lg = [
+    { i: 'Canvas', x: 0, y: 0, w: 1, h: 4},
+    { i: 'ModelPred', x: 1, y: 0, w: 2, h: 6.75},
+    { i: 'Stats', x: 0, y: 1.5, w: 1, h: 6},
+
+]
+
+export const md = [
+    { i: 'Canvas', x: 0, y: 0, w: 2, h: 4},
+    { i: 'ModelPred', x: 0, y: 1.5, w: 2, h: 6.75},
+    { i: 'Stats', x: 0, y: 1.5, w: 2, h: 6},
+
+]
