e2bbeb49fe6d311882e66133f5b178afb3c0d876
Thomas Pham <phamthomas19@gmail.com>
2025-08-19 21:39:56 -0500

    section 1

--- components/pages/notesPage/notes/2/Note.tsx
+++ components/pages/notesPage/notes/2/Note.tsx
@@ -5,11 +5,33 @@ const Note = () => {
     return (
         <div className={styles.container}>
             <div className={styles.contentContainer}>
-                <div className={styles.title}><span>Welcome to my website</span></div>
+                <div className={styles.title}><span>How I made this website</span></div>
                 <div className={styles.date}><span>May 02, 2024</span></div>
-                <div className={styles.content}>
+                <section className={styles.content}>
                     
-                </div>
+                    <div className={styles.sectionHeader}><span>Motivation</span></div>
+                    <p className={styles.p}>
+                        Despite my interest in programming, for the longest time I avoided web development.
+                        My first attemps at learning HTML, CSS, and JS were <em>pre-ChatGPT</em>, where I would
+                        spend hours in 10-hour courses and still feel like I couldn't build anything original.
+                        The phrase "tutorial hell" pretty much sums it up.
+                    </p>
+                    <br />
+                    <p className={styles.p}>
+                        Somehow, React was more intuitive to me than HTML or JS. Things finally started
+                        making sense in the summer of 2023 during my internship where most of my work was
+                        on front end tasks. Thankfully, ChatGPT was also around that time, or I probably would have
+                        struggled a lot. I gained a lot of confidence from that experience, and I realized
+                        web development wasn't so bad after all
+                    </p>
+                    <br />
+                    <p className={styles.p}>
+                        Since then, I've learned a ton and wanted to challenge myself by building a website
+                        from scratch. I hope to share how that process went here.
+                    </p>
+                
+
+                </section>
             </div>
         </div>
     )
